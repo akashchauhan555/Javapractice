f74e32a23e5ef70d3689bfb6064d19a1c096356b
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-07-04 09:52:57 +0530

    firstdone

---
+++ first.java
@@ -0,0 +1,10 @@
+public class first {
+  public static void main(String args[]) {
+    System.out.print("The sum of these numbers:");
+    int num1 = 4;
+    int num2 = 7;
+    int num3 = 6;
+    int sum = num1 + num2 + num3;
+    System.out.println(sum);
+  }
+}
